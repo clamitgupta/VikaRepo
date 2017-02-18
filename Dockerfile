4d056d56ec521c79eb4e56dfa0a35517f6f4c550
Amit gupta <amitgupta@clerisysolutions.com>
2017-02-18 13:40:48 +0530

    database

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM php:5.6.29-apache
 COPY src/ /var/www/html
 RUN a2enmod rewrite
+
+run -d -e MYSQL_ROOT_PASSWORD=tracking -e MYSQL_DATABASE=tracking -p 3307:3307 --name mysql mysql:latest
 Expose 80
 
--- src/index.php
+++ src/index.php
@@ -1,2 +1,33 @@
 <?php
 echo 'This is testing git and docker files and modified on date 18-2-2017.';
+
+
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+
+$servername = "172.17.0.2";
+$username = "root";
+$password = "tracking";
+$dbname = "tracking";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, username, email, first_name FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Username: " . $row["username"]. "<br/> Email: " . $row["email"]. "<br/>firstName: ". $row['first_name'] ."<br><br/>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
